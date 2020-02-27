74bdc5ac138cb635106875e37e49ba2cbce7ac0f
Сергей Степанов <imbashamba@gmail.com>
2020-02-27 12:32:34 +0300

    fix "-1111: Precision is over the maximum defined for this asset."

--- Binance_Futures_python-master/binance_f/impl/utils/urlparamsbuilder.py
+++ Binance_Futures_python-master/binance_f/impl/utils/urlparamsbuilder.py
@@ -13,7 +13,7 @@ class UrlParamsBuilder(object):
             if isinstance(value, list):
                 self.param_map[name] = json.dumps(value)
             elif isinstance(value, float):
-                self.param_map[name] = ('%.20f' % (value))[slice(0, 16)].rstrip('0').rstrip('.')
+                self.param_map[name] = ('%.20f' % (value))[slice(0, 5)].rstrip('0').rstrip('.')
             else:
                 self.param_map[name] = str(value)
     def put_post(self, name, value):
